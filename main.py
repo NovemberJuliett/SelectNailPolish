afa409988247e5a8b402526b72ec3a55f01abff2
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-12-17 13:24:40 +0200

    start new project

---
+++ main.py
@@ -0,0 +1,5 @@
+mood_input = input("What сolor do you want to wear today? ")
+answer = ["I don't know", "don't know", "idk", "dont know"]
+if mood_input in answer:
+    print("I will help you")
+color_input = input("Are you ready for random color?")
